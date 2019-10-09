1e063242f31b6175f6f96bdf2934620e757a80a1
Brianna Psanos <bpsanos@yahoo.com>
2019-10-09 00:13:27 -0500

    ready to submit

--- liri.js
+++ liri.js
@@ -36,5 +36,56 @@ if (args[0] === "do-what-it-says") {
         }
 
         dataArr = data.split(",");
-    }
-}
+        if (dataArr[0] === "movie-this") {
+            if (dataArr[1] === undefined) {
+                getMovie("Mr. +Nobody")
+            }
+        };
+    
+        if (dataArr[0] === "spotify-this-song") {
+            if (dataArr[1] === undefined) {
+                spotifySong("Bye Bye Bye")
+            }
+            else {
+                spotifySong(dataArr[1])
+            }
+        };
+    });
+};
+
+function spotifySong(songName) {
+    spotify.search({ type: 'track', query: songName, limit: 5 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        data.tracks.items.forEach(function (element) {
+            console.log("");
+            console.log(`Artist: ${element.artists[0].name}`);
+            console.log(`Song: ${songName}`);
+            console.log(`Spotify Preview Link: ${element.preview_url}`);
+            console.log(`Album: ${element.album.name}`);
+        });
+    })
+};
+
+function getMovie(movieName) {
+
+    axios
+        .get(`http://omdbapi.com/?t=${movieName}&apikey=cf1c54e0`)
+        .then(function (movie) {
+
+            console.log("");
+            console.log(`Title: ${movie.data.Title}`);
+            console.log(`Released: ${movie.data.Year}`);
+            console.log(`IMDB Rating: ${movie.data.Ratings[0].Value}`);
+            console.log(`Rotten Tomatoes Rating: ${movie.data.Ratings[1].Value}`);
+            console.log(`Produced in: ${movie.data.Country}`);
+            console.log(`Plot: ${movie.data.Plot}`);
+            console.log(`Starring: ${movie.data.Actors}`);
+
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+};
